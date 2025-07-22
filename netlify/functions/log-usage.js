6db23142148c4284fbc113b725f99f4e0c05a312
Tuomo Keltto <tuomo.keltto@metso.com>
2025-07-22 16:13:51 +0300

    adding logs to gsheet

--- netlify/functions/log-usage.js
+++ netlify/functions/log-usage.js
@@ -1,22 +1,22 @@
 // netlify/functions/log-usage.js
 
-const fetch = require('node-fetch');
+//const fetch = require('node-fetch');
 
-exports.handler = async (event) => {
-    const data = JSON.parse(event.body);
+//exports.handler = async (event) => {
+//    const data = JSON.parse(event.body);
 
-    const response = await fetch('https://script.google.com/macros/s/AKfycbwTXYksaEAhemv1kF3Js7QKlpVBnA9N3XVdwnB-fhxnX-tB_D9rtfhyjMrJITbBrh8/exec', {
-        method: 'POST',
-        body: JSON.stringify({
-            domain: data.domain,
-            feature: data.feature,
-            info: data.info || ""
-        }),
-        headers: { 'Content-Type': 'application/json' }
-    });
+//    const response = await fetch('https://script.google.com/macros/s/AKfycbwTXYksaEAhemv1kF3Js7QKlpVBnA9N3XVdwnB-fhxnX-tB_D9rtfhyjMrJITbBrh8/exec', {
+//        method: 'POST',
+//        body: JSON.stringify({
+//            domain: data.domain,
+//            feature: data.feature,
+//            info: data.info || ""
+//        }),
+//        headers: { 'Content-Type': 'application/json' }
+//    });
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Logged to Google Sheets" }),
-    };
-};
+//    return {
+//        statusCode: 200,
+//        body: JSON.stringify({ message: "Logged to Google Sheets" }),
+//    };
+//};
